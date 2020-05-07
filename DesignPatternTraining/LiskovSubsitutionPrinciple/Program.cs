aa3507cc314c9c1b602dbc5133d769af108fad71
Tomasz Golec <tgolec@sii.pl>
2020-05-07 15:57:51 +0200

    initial problem to resolve using liskov subsitution principle

--- DesignPatternTraining/LiskovSubsitutionPrinciple/Program.cs
+++ DesignPatternTraining/LiskovSubsitutionPrinciple/Program.cs
@@ -1,21 +1,66 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace LiskovSubsitutionPrinciple
 {
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+
+    }
+
+    public class Square: Rectangle
+    {
+        public new int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public new int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
     class Program
     {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Rectangle rc = new Rectangle(2,3);
+            WriteLine($"{rc} has area {Area(rc)}");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Square sq = new Square();
+            sq.Width = 4;
+            WriteLine($"{sq} has area {Area(sq)}");
+
+            //that the problem, now square do not behave like square
+            Rectangle sq2 = new Square();
+            sq2.Width = 4;
+            WriteLine($"{sq} has area {Area(sq2)}");
+
+            ReadKey();
         }
     }
 }
