4b09a4f5c917f73d14b1b20f85459f0a27550656
Tomasz Golec <tgolec@sii.pl>
2020-05-08 10:55:28 +0200

    14. Fluent Builder Inheritence with Recursive Generics - soulution // you should know more about generics construction

--- DesignPatternTraining/FluentBuilderWithINheritance/Program.cs
+++ DesignPatternTraining/FluentBuilderWithINheritance/Program.cs
@@ -14,39 +14,57 @@ namespace FluentBuilderWithINheritance
             public string Name;
             public string Position;
 
+            public class Builder:PersonJobBuilder<Builder>
+            {
+                
+            }
+
+            public static Builder New => new Builder();
+
             public override string ToString()
             {
                 return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
             }
         }
 
-        public class PersonInfoBuilder
+
+        public abstract class PersonBuilder
         {
             protected Person person = new Person();
-
-            public PersonInfoBuilder Called(string name)
+            public Person Build()
+            {
+                return person;
+            }
+        }
+        public class PersonInfoBuilder<SELF> : PersonBuilder
+        where SELF : PersonInfoBuilder<SELF>
+        {
+            public SELF Called(string name)
             {
                 person.Name = name;
-                return this;
+                return (SELF) this;
             }
         }
 
-        public class PersonJobBuilder : PersonInfoBuilder 
+        public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>>
+        where SELF : PersonJobBuilder<SELF>
         {
-            public PersonJobBuilder WorksAsA(string position)
+            public SELF WorksAsA(string position)
             {
                 person.Position = position;
-                return this;
+                return (SELF) this;
             }
         }
 
         static void Main(string[] args)
         {
             //here we have a problem because fluent builder does not work if you use this with inheritance
-            var builder = new PersonJobBuilder();
-            builder.Called("dmitri")
-                .WorksAsA
 
+            var me =  Person.New
+                .Called("dmitri")
+                .WorksAsA("engineer")
+                .Build();
+            WriteLine(me);
             ReadKey();
         }
     }
