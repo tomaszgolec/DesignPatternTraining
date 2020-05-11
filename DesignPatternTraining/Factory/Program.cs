d3cc4a3d2b18b0640c2e3047d485e9f86a6a528e
Tomasz Golec <tgolec@sii.pl>
2020-05-11 12:36:54 +0200

    23. Inner Factory - eplenation how to keep constructor private when we use factory class

--- DesignPatternTraining/Factory/Program.cs
+++ DesignPatternTraining/Factory/Program.cs
@@ -6,18 +6,7 @@ namespace Factory
     //based ont SRP we would like to move creation of objects to separate class because creation is different responsibility 
     //we can use factory methods in small classes when is small amount of this kind of methods
 
-    public static class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
+   
 
     public class Point
     {
@@ -25,8 +14,10 @@ namespace Factory
 
         private double x, y;
 
-        //is private now
-        public Point(double x, double y)
+        //if we use internal we avoid inappropriate usage of this constructor for clients using this library
+        //but still someone can us it in wrong way inside library 
+        //so good idea is put factory class inside this class and then we can keep constructor private! :)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -36,6 +27,39 @@ namespace Factory
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
         }
+
+        public static Point Origin => new Point(0,0); //this is a property  
+
+        public static Point Origin2 = new Point(0,0); //this is better because is initialising once but if you need always new object ten you can use line above
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+        //if is some reason why factory cannot be static then you can use alternatively solution below
+        //but better approach is static, if you need static version then probably you hav bad design 
+        //public  static PointFactory Factory => new PointFactory();
+        //public class PointFactory
+        //{
+        //    public Point NewCartesianPoint(double x, double y)
+        //    {
+        //        return new Point(x, y);
+        //    }
+
+        //    public Point NewPolarPoint(double rho, double theta)
+        //    {
+        //        return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        //    }
+        //}
     }
 
 
@@ -45,7 +69,7 @@ namespace Factory
         static void Main(string[] args)
         {
             //this patter is just API improvement similarly to builder
-            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
 
             WriteLine(point);
             ReadKey();
