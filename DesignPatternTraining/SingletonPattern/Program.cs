864a3d386df7c80f66f090b6218f26594bf1a443
Tomasz Golec <tgolec@sii.pl>
2020-05-12 11:15:55 +0200

    34. Testability Issues

--- DesignPatternTraining/SingletonPattern/Program.cs
+++ DesignPatternTraining/SingletonPattern/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using MoreLinq;
+using NUnit.Framework;
 using static System.Console;
 
 namespace SingletonPattern
@@ -17,11 +18,19 @@ namespace SingletonPattern
     {
         private Dictionary<string, int> capitals;
 
+        //just for experimental purpose 
+        private static int instanceCount;
+        public static int Count => instanceCount;
+
         private SingletonDatabase()
         {
+            instanceCount++;
             WriteLine("initializing database");
 
-            capitals = File.ReadAllLines("capitals.txt")
+            string fileLocation = Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName,
+                "capitals.txt");
+
+            capitals = File.ReadAllLines(fileLocation)
                 .Batch(2)
                 .ToDictionary(
                     list => list.ElementAt(0).Trim(), 
@@ -45,17 +54,50 @@ namespace SingletonPattern
         public static SingletonDatabase Instance => instance.Value;
     }
 
-    class Program
+    public class SingletonRecordFinder
     {
-
-        static void Main(string[] args)
+        public int GetTotalPopulation(IEnumerable<string> names)
         {
-            var db = SingletonDatabase.Instance;
-
-            var city = "Tokyo";
-            WriteLine($"{city} has population {db.GetPopulation(city)}");
-
-            ReadKey();
+            int result = 0;
+            foreach (var name in names)
+                result += SingletonDatabase.Instance.GetPopulation(name);
+            return result;
         }
     }
+
+
+    [TestFixture]
+    public class SingletonTest
+    {
+        [Test]
+        public void IsSingletonTest()
+        {
+            var db = SingletonDatabase.Instance;
+            var db2 = SingletonDatabase.Instance;
+            Assert.That(db,Is.SameAs(db2));
+            Assert.That(SingletonDatabase.Count,Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SingletonTotalPopulationTest()
+        {
+            var rf = new SingletonRecordFinder();
+            var names = new[] {"Seoul", "Mexico City"};
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp, Is.EqualTo(17500000+17400000));
+        }
+    }
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var db = SingletonDatabase.Instance;
+
+    //        var city = "Tokyo";
+    //        WriteLine($"{city} has population {db.GetPopulation(city)}");
+
+    //        ReadKey();
+    //    }
+    //}
 }
