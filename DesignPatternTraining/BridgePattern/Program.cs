ffc2c413d227fc50a13c9aba6494965e822352cb
Tomasz Golec <tgolec@sii.pl>
2020-05-13 10:29:43 +0200

    Coding Exercise 6: Bridge Coding Exercise - important comments inside

---
+++ DesignPatternTraining/BridgePattern/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Reflection.Metadata;
+using Autofac;
+using static System.Console;
+
+namespace BridgePattern
+{
+
+    interface IRenderer
+    {
+        void RenderCircle(float radius);
+    }
+
+    class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing a circle of radius {radius}");
+        }
+
+    }
+
+    class RasterRenderer : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            WriteLine($"Drawing pixels for circle with radius {radius}");
+        }
+    }
+
+    abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        protected Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    class Circle : Shape
+    {
+        private float radius;
+
+        public Circle(IRenderer renderer, float radius) : base(renderer)
+        {
+            this.radius = radius;
+        }
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IRenderer renderer = new RasterRenderer();
+            //IRenderer renderer = new VectorRenderer();
+            //var circle = new Circle(renderer,5);
+
+
+            //circle.Draw();
+            //circle.Resize(2);
+            //circle.Draw();
+
+            var cb = new ContainerBuilder();
+            cb.RegisterType<VectorRenderer>().As<IRenderer>()
+                .SingleInstance();
+
+            cb.Register((c, p) => 
+                new Circle(c.Resolve<IRenderer>(),
+                    p.Positional<float>(0)));
+
+            using (var c = cb.Build())
+            {
+                var circle = c.Resolve<Circle>(
+                    new PositionalParameter(0,5.0f)
+                );
+
+
+                circle.Draw();
+                circle.Resize(2.0f);
+                circle.Draw();
+            }
+
+
+            ReadKey();
+        }
+    }
+}
---
+++ DesignPatternTraining/Test7/Program.cs
@@ -0,0 +1,84 @@
+﻿using static System.Console;
+
+namespace Test7
+{
+
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+    }
+
+
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public class Triangle : Shape
+    {
+        private IRenderer renderer;
+        public Triangle(IRenderer renderer)
+        {
+            this.renderer = renderer;
+            Name = "Triangle";
+        }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {renderer.WhatToRenderAs}";
+        }
+    }
+
+    public class Square : Shape
+    {
+        private readonly IRenderer renderer;
+
+        public Square(IRenderer renderer)
+        {
+            this.renderer = renderer;
+            Name = "Square";
+        }
+
+        public override string ToString()
+        {
+            return $"Drawing {Name} as {renderer.WhatToRenderAs}";
+        }
+    }
+
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs { get; } = "lines";
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs { get; } = "pixels";
+    }
+
+
+    //we have an old approach below, on first sight bridge pattern above can looks like unnecessary
+    //but if you will imagine that we have 10 implementations of shape and for each, we have the next two implementation
+    //like below then we will have more than 20 classes(int bridge pattern we have only 12) and worst is we must
+    //change implementation in every class which inherit from square or other classes
+    //even if we have the same function inside so BRIDGE PATTERN IS GOOD APPROACH WHEN WE HAVE MULTIPLE CLASSES WITH
+    //CARTESIAN-PRODUCT DUPLICATION, OBVIOUSLY IT WILL BE GOOD SOLUTION EVEN IN ADVANCE
+    //public class VectorSquare : Square
+    //{
+    //    public override string ToString() => "Drawing {Name} as lines";
+    //}
+
+    //public class RasterSquare : Square
+    //{
+    //    public override string ToString() => "Drawing {Name} as pixels";
+    //}
+
+    // imagine VectorTriangle and RasterTriangle are here too
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
