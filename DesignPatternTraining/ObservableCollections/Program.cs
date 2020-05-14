0fefcfb12acacc062102dff305eb4273628026df
Tomasz Golec <tomaszgolec1993@gmail.com>
2020-05-14 18:25:25 +0200

    INotifyPropertyChanged example from (117. Observable Collections)

---
+++ DesignPatternTraining/ObservableCollections/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ObservableCollections.Annotations;
+using static System.Console;
+
+namespace ObservableCollections
+{
+
+    public class Market : INotifyPropertyChanged
+    {
+        private float volatility;
+
+
+        public float Volatility
+        {
+            get => volatility;
+            set
+            {
+                if (value.Equals(volatility)) return;
+                volatility = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var market = new Market();
+
+            market.PropertyChanged += (sender, eventargs) =>
+            {
+                if (eventargs.PropertyName == "Volatility")
+                {
+                    WriteLine($"Volatility has new value : {market.Volatility}");
+                }
+            };
+            ReadKey();
+            market.Volatility = 10;
+
+            ReadKey();
+        }
+    }
+}
