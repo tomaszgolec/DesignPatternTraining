087cd87912e8006c0386b9a6cd8e0b89254d2b63
Tomasz Golec <tomaszgolec1993@gmail.com>
2020-05-14 13:40:52 +0200

    113. Observer via the 'event' Keyword

---
+++ DesignPatternTraining/Observer_Via_The_Event_Keyword/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using static System.Console;
+
+namespace Observer_Via_The_Event_Keyword
+{
+    public class FallsIllEventArgs
+    {
+        public string Address;
+    }
+    public class Person
+    {
+        public void CatchACold()
+        {
+            FallsIll?.Invoke(this,new FallsIllEventArgs { Address = "123 London Road"});
+        }
+
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.FallsIll += CallDoctor;
+
+            person.CatchACold();
+
+            person.FallsIll -= CallDoctor;
+
+            ReadKey();
+        }
+
+        private static void CallDoctor(object sender, FallsIllEventArgs e)
+        {
+            WriteLine($"A doctor has been called to {e.Address}");
+        }
+    }
+}
