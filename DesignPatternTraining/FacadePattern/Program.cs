d76d03ec27e32b502cc48a85098f74fa41dbc61d
Tomasz Golec <tomaszgolec1993@gmail.com>
2020-05-26 14:13:04 +0200

    65. Faccade

---
+++ DesignPatternTraining/FacadePattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace FacadePattern
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            // All example is in the course, nothing to write
+            ReadKey();
+        }
+    }
+}
