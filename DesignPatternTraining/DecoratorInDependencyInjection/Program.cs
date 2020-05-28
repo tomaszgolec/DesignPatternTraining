fbfb239e4eb041e351e9f166bec57dc4540ff48e
Tomasz Golec <tomaszgolec1993@gmail.com>
2020-05-28 16:11:36 +0200

    62. Decorator in Dependency Injection

---
+++ DesignPatternTraining/DecoratorInDependencyInjection/Program.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using static System.Console;
+
+namespace DecoratorInDependencyInjection
+{
+    class Program
+    {
+
+        public interface IReportingService
+        {
+            void Report();
+        }
+
+        public class ReportingService : IReportingService
+        {
+            public void Report()
+            {
+                WriteLine("Here is your report");
+            }
+        }
+
+        public class ReportingServiceWithLogging : IReportingService
+        {
+            private IReportingService decorated;
+
+            public ReportingServiceWithLogging(IReportingService decorated)
+            {
+                this.decorated = decorated;
+            }
+            public void Report()
+            {
+                WriteLine("Commencing log log...");
+                decorated.Report();
+                WriteLine("Ending log...");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            var b = new ContainerBuilder();
+
+            b.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+
+            b.RegisterDecorator<IReportingService>(
+                (context, service) => new ReportingServiceWithLogging(service), "reporting");
+
+            using (var c = b.Build())
+            {
+                var r = c.Resolve<IReportingService>();
+                r.Report();
+            }
+
+            ReadKey();
+        }
+    }
+}
